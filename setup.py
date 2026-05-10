8cd13a41b5ca47c7746f8ec35375b98491d79184
Tushar <tushaar@example.com>
2026-05-10 16:23:32 +0530

    project structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is end to end mlops project',
+    author='Tushar Choudhary',
+    license='MIT',
+)
