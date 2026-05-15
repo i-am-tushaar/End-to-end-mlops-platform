38a7538e3afa88ed4c0aeb2e3b9f801e0ae53677
Tushar <tushaar@example.com>
2026-05-15 17:52:02 +0530

    configured DVC pipeline with dvc.yaml and params.yaml and validated successful end-to-end run via dvc repro

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -75,7 +75,7 @@ def main():
         test_size = params['data_ingestion']['test_size']
         # test_size = 0.2
         
-        df = load_data(data_url='https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/data.csv')
+        df = load_data(data_url='https://raw.githubusercontent.com/i-am-tushaar/Datasets/refs/heads/main/data.csv')
         # s3 = s3_connection.s3_operations("bucket-name", "accesskey", "secretkey")
         # df = s3.fetch_file_from_s3("data.csv")
 
