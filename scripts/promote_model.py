c376e43a2333578c7c2fd6dd4e1610da49fcb1a6
Tushar <tushaar@example.com>
2026-05-19 18:09:10 +0530

    CICD run - 6

--- scripts/promote_model.py
+++ scripts/promote_model.py
@@ -6,24 +6,24 @@ import dagshub
 
 def promote_model():
     # Set up DagsHub credentials for MLflow tracking
-    # dagshub_token = os.getenv("CAPSTONE_TEST")
-    # if not dagshub_token:
-    #     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+    dagshub_token = os.getenv("CAPSTONE_TEST")
+    if not dagshub_token:
+        raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-    # os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-    # os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-    # dagshub_url = "https://dagshub.com"
-    # repo_owner = "tushar.dataexpert"
-    # repo_name = "End-to-end-mlops-platform"
+    dagshub_url = "https://dagshub.com"
+    repo_owner = "tushar.dataexpert"
+    repo_name = "End-to-end-mlops-platform"
 
-    # # Set up MLflow tracking URI
-    # mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+    # Set up MLflow tracking URI
+    mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
     # Below code block is for local use
     # -------------------------------------------------------------------------------------
-    mlflow.set_tracking_uri('https://dagshub.com/tushar.dataexpert/End-to-end-mlops-platform.mlflow')
-    dagshub.init(repo_owner='tushar.dataexpert', repo_name='End-to-end-mlops-platform', mlflow=True)    
+    # mlflow.set_tracking_uri('https://dagshub.com/tushar.dataexpert/End-to-end-mlops-platform.mlflow')
+    # dagshub.init(repo_owner='tushar.dataexpert', repo_name='End-to-end-mlops-platform', mlflow=True)    
 
     client = mlflow.MlflowClient()
 
